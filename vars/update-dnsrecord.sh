42c84b571ef1a0278b061623dea4f2c6bbfd8a99
kiran1616 <kirankumar7163@gmail.com>
2023-02-02 17:59:45 +0530

    updated script

---
+++ vars/update-dnsrecord.sh
@@ -0,0 +1,17 @@
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=Jenkins-master" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+echo '
+{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "jenkins.kiranprav.link",
+      "Type": "A",
+      "TTL": 15,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
+
+ZONE_ID="Z05909301HWY2LI69YHHG"
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
